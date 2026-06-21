9cbb0a1d579799e251e8227a2a69850170a6a4a5
MystiqueMahir <notmahirkhan@gmail.com>
2026-06-21 11:11:24 +0530

    Add StudentManager class header file

---
+++ StudentManager.h
@@ -0,0 +1,14 @@
+#ifndef STUDENTMANAGER_H
+#define STUDENTMANAGER_H
+
+class StudentManager
+{
+public:
+    void addStudent();
+    void displayStudents();
+    void searchStudent();
+    void updateStudent();
+    void deleteStudent();
+};
+
+#endif
